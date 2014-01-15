4e0f80dc99967a1c9b21b6f0d96e35a85f099ca0
tehaaron <aaronselvig@gmail.com>
2014-01-15 14:52:36 -0800

    fixed displaying localStorage
    page now loads existing storage onLoad and new submitted items show up
    and persist.

--- js/app.js
+++ js/app.js
@@ -10,12 +10,12 @@ window.onload = function() {
 
 
 	var btnSave = document.getElementById('btnSave');
-	btnSave.addEventListener('click', add, false);
+	btnSave.addEventListener('click', Add, false);
 
-
+	List();
 };
 
-var add = function(){
+var Add = function(){
 	var SME = JSON.stringify({
 		firstname:document.getElementById('firstname').value,
 		lastname:document.getElementById('lastname').value,
@@ -25,6 +25,20 @@ var add = function(){
 
 	rolodex.push(SME);
 	localStorage.setItem('rolodex', JSON.stringify(rolodex));
+
+	for (var key in SME) {
+		var person = JSON.parse(SME[key]);
+		var div = document.getElementById('list-display').lastChild;
+		div.innerHTML = div.innerHTML + "<div class='sme-entry'><ul><li>"+person.subject+"</li><li>"+person.firstname+" "+person.lastname+"</li><li>"+person.email+"</li></ul></div>";
+	}
+
 	alert("Saved");
 };
 
+var List = function(){
+	for (var key in rolodex) {
+		var person = JSON.parse(rolodex[key]);
+		var div = document.getElementById('list-display');
+		div.innerHTML = div.innerHTML + "<div class='sme-entry'><ul><li>"+person.subject+"</li><li>"+person.firstname+" "+person.lastname+"</li><li>"+person.email+"</li></ul></div>";
+	}
+};
